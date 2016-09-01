d178158dda4478d6a70665c9a674c39a2bbc53d1
Jinx <jinxlovejinx@vip.qq.com>
2016-09-01 10:07:13 +0800

    文件初始化提交

---
+++ temp/src/com/jinx/test/PrintTest.java
@@ -0,0 +1,12 @@
+package com.jinx.test;
+
+/**
+ * 输出测试程序
+ */
+public class PrintTest {
+
+    public static void main(String[] args){
+        System.out.print("This is just a test !");
+    }
+
+}
