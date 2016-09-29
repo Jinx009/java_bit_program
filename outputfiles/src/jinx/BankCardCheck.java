f71718da9ef3c3692fec9d62ef4f569153cadc67
Jinx <jinxlovejinx@vip.qq.com>
2016-09-29 10:49:51 +0800

    JAVA校验银行卡号正确性

--- outputfiles/src/jinx/BankCardCheck.java
+++ outputfiles/src/jinx/BankCardCheck.java
@@ -3,33 +3,50 @@ package jinx;
 
 /**
  * Created by jinx on 9/28/16.
- * 目前仅能校验18位银行卡
+ * 校验银行卡
  */
 public class BankCardCheck {
+
+    public static String check(String bankCardNum){
+        int sumOdd = 0,sumEven = 0;
+        if(bankCardNum==null||"".equals(bankCardNum))
+            return "银行卡号不能为空!";
+
+        int length = bankCardNum.length();
+        String lastNum = bankCardNum.substring(length-1,length);
+        int temp = 0;
+        bankCardNum = bankCardNum.substring(0,length-1);
+        for (int i = length-1; i >=1; i--) {
+            temp = Integer.valueOf(bankCardNum.substring(i-1,i));// 从末位一位开始提取，每一位上的数值
+            if(length%2==1){
+                if(i%2==0){
+                    temp = 2*temp;
+                    if(temp>10)
+                        temp = temp - 9;
+                    sumEven += temp;
+                } else{
+                    sumOdd += temp;
+                }
+            }else{
+                if(i%2==1){
+                    temp = 2*temp;
+                    if(temp>10)
+                        temp = temp - 9;
+                    sumEven += temp;
+                } else{
+                    sumOdd += temp;
+                }
+            }
+        }
+        if ((sumOdd + sumEven+Integer.valueOf(lastNum)) % 10 == 0){
+            return "success";
+        }
+        else{
+            return "银行卡号不合法!";
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Please input your credit card number:");
-        int sumOdd = 0;
-        int sumEven = 0;
-        String number = "621758080000007860";
-        int length = number.length();
-        int[] wei = new int[length];
-        for (int i = 0; i < number.length(); i++) {
-            wei[i] = Integer.parseInt(number.substring(length - i - 1, length- i));// 从最末一位开始提取，每一位上的数值
-            System.out.println("第" + i + "位数字是：" + wei[i]);
-        }
-        for (int i = 0; i < length / 2; i++) {
-        sumOdd += wei[2 * i];
-        if ((wei[2 * i + 1] * 2) > 9)
-            wei[2 * i + 1] = wei[2 * i + 1] * 2 - 9;
-        else
-            wei[2 * i + 1] *= 2;
-            sumEven += wei[2 * i + 1];
-        }
-        System.out.println("奇数位的和是：" + sumOdd);
-        System.out.println("偶数位的和是：" + sumEven);
-        if ((sumOdd + sumEven) % 10 == 0)
-            System.out.println("Recept.");
-        else
-            System.out.println("Can not recept.");
+        System.out.println(check("6217580800000078105"));
     }
 }
