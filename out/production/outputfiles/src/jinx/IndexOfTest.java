ad59679e92d325b952b59da9b0488ca9865a10fa
Jinx <jinxlovejinx@vip.qq.com>
2016-10-08 12:17:52 +0800

    JAVA字符串包含另外一个字符串判断

---
+++ out/production/outputfiles/src/jinx/IndexOfTest.java
@@ -0,0 +1,13 @@
+package jinx;
+
+/**
+ * Created by jinx on 10/8/16.
+ */
+public class IndexOfTest {
+
+    public static void main(String[] args){
+        String test = "1111*2222";
+        System.out.print(test.indexOf("3"));
+    }
+
+}
---
+++ outputfiles/src/jinx/IndexOfTest.java
@@ -0,0 +1,13 @@
+package jinx;
+
+/**
+ * 字符串包含另外一个字符串判断
+ */
+public class IndexOfTest {
+
+    public static void main(String[] args){
+        String test = "1111*2222";
+        System.out.print(test.indexOf("3"));
+    }
+
+}
